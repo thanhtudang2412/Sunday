5fd89156d9d658fcae210326c1ab7f0ca963e555
thanhtudang2412 <thanhtudang2412@users.noreply.github.com>
2021-02-07 15:54:53 +0700

    Day 1

---
+++ Sunday/app/src/main/java/com/example/sunday/RegisterActivity.java
@@ -0,0 +1,27 @@
+package com.example.sunday;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Create Account");
+
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
+    }
+}
